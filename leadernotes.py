27a5427a1f046d54769fa5cd3bdd586ee3b6ae61
W. Patrick Jones <wpmjones@users.noreply.github.com>
2018-03-07 16:23:55 -0600

    Notify leaders of problem players
    moved common calls to functions

--- leadernotes.py
+++ leadernotes.py
@@ -5,19 +5,29 @@ import pymssql
 from datetime import datetime
 from config import settings
 
+def fetch_notification_count(memberTag, clanTag):
+  cursor = conn1.cursor()
+  cursor.execute('SELECT COUNT(timestamp) AS reported, clanTag, memberTag FROM rcs_notify WHERE memberTag = %s AND clanTag = %s GROUP BY clanTag, memberTag', (memberTag, clanTag))
+  fetch = cursor.fetchall()
+  if cursorMain.rowcount == 0:
+    return 0
+  else:
+    return fetch[0][0]
+
+def fetch_discord_tag(clanTag):
+  cursor = conn1.cursor()
+  cursor.execute('SELECT clanTag, discordTag FROM rcs_data WHERE clanTag = %s', clanTag)
+  fetch = cursor.fetchall()
+  return fetch[0][1]
+
 # Open SQL connection
 conn1=pymssql.connect(settings['database']['server'], settings['database']['username'], settings['database']['password'], settings['database']['database'])
 cursorMain=conn1.cursor()
-cursorDict=conn1.cursor(as_dict=True)
 
 # Fetch list of RCS Clans
 cursorMain.execute('SELECT clanTag FROM rcs_data')
 fetch = cursorMain.fetchall()
-# I don't remember what error I was having, but I wasn't able to use fetch as a list later, so this
-# section just puts the fetch into a python list
-rcsClans = []
-for clan in fetch:
-  rcsClans.append(clan[0])
+rcsClans = [clan[0] for clan in fetch]
 
 # Pull message history from the RCS Discord #leader-notes channel
 headers = {'Accept':'application/json','Authorization':'Bot ' + settings['discord']['leaderToken']}
@@ -27,15 +37,14 @@ stringdata = str(r.json())
 
 # find text in message history that is preceeded by 'Tag: ' or '#'
 regex = ur"[tT]ag:\s[a-zA-Z0-9]+|#[a-zA-Z0-9]{6,}"
-matches = re.finditer(regex, stringdata)
 
 banSet = set()
-for match in matches:
+for match in re.finditer(regex, stringdata):
   if match.group() != '#':
     banSet.add(match.group().upper().replace('TAG: ','#'))
   else:
     banSet.add(match.group())
-# Convert to unique list (some players have multiple warnings
+# Convert to unique list (some players have multiple warnings)
 banList = list(banSet)
 
 headers = {'Accept':'application/json','Authorization':'Bearer ' + settings['supercell']['apiKey']}
@@ -49,18 +58,11 @@ for member in banList:
   # Used a try because if someone is not in a clan, the JSON will not have ['clan']['tag']
   try:
     if data['clan']['tag'][1:] in rcsClans:
-      cursorMain.execute('SELECT COUNT(timestamp) AS reported, clanTag, memberTag FROM rcs_notify WHERE memberTag = %s AND clanTag = %s GROUP BY clanTag, memberTag', (data['ta$
-      fetch = cursorMain.fetchall()
-      if cursorMain.rowcount == 0:
-        reported = 0
-      else:
-        reported = fetch[0][0]
+      reported = fetch_notification_count(data['tag'][1:], data['clan']['tag'][1:])
       if reported < 3:
-        cursorDict.execute('SELECT clanTag, discordTag FROM rcs_data WHERE clanTag = %s', data['clan']['tag'][1:])
-        clan = cursorDict.fetchone()
-        print(data['name'] + ' (' + data['tag'] + ') is in ' + data['clan']['name'] + '. Please check #leader-notes for details.')
+        discordTag = fetch_discord_tag(data['clan']['tag'][1:])
         payload = {
-          'content' : '<@!' + clan['discordTag'] + '> ' + data['name'].encode('ascii','ignore') + ' (' + data['tag'] + ') is in ' + data['clan']['name'] + '. Please search for$
+          'content' : '<@!' + discordTag + '> ' + data['name'].encode('ascii','ignore') + ' (' + data['tag'] + ') is in ' + data['clan']['name'] + '. Please search for `in:leader-notes ' + data['tag'] + '` for details.'
         }
         r = requests.post(settings['discord']['leaderDanger'], json=payload)
         cursorMain.execute('INSERT INTO rcs_notify VALUES (%s, %s, %s)', (datetime.now().strftime('%m-%d-%Y %H:%M:%S'), clan['clanTag'], data['tag'][1:]))
